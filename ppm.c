5ce3012ce274bb42c44dcf71c8218cedcfe489a5
Nicholas Léonard <nick@nikopia.org>
2017-04-20 14:36:45 -0400

    fix narrow with math.floor (#220)

--- ppm.c
+++ ppm.c
@@ -1,13 +1,13 @@
 
-#include <TH.h>
 #include <luaT.h>
+#include <TH.h>
 
 #define torch_(NAME) TH_CONCAT_3(torch_, Real, NAME)
 #define torch_Tensor TH_CONCAT_STRING_3(torch., Real, Tensor)
 #define libppm_(NAME) TH_CONCAT_3(libppm_, Real, NAME)
 
 /* Get the next character in the file, skipping over comments, which
- * start with a # and continue to the end of the line. 
+ * start with a # and continue to the end of the line.
  */
 static char ppm_getc(FILE *fp)
 {
